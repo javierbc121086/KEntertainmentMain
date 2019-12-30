d46929d170001ec3dea76936b7c3e0bfaf8e0618
javier.bolanos.ironbit <javier.bolanos@ironbit.com.mx>
2019-12-29 20:37:40 -0600

    Initial Commit

---
+++ KEntertainmentMain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KEntertainmentMain
+//
+//  Created by Javier Bolaños on 12/29/19.
+//  Copyright © 2019 Javier Bolaños. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
